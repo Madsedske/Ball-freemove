9a872d55b4371719af58ab74b8e2d5b520f55298
Madsedske <88845582+Madsedske@users.noreply.github.com>
2021-12-09 12:50:38 +0100

    Næsten perfekt bounds

--- Ball freemove/Form1.Designer.cs
+++ Ball freemove/Form1.Designer.cs
@@ -36,25 +36,34 @@ namespace Ball_freemove
             this.pictureBoxYdre1 = new System.Windows.Forms.PictureBox();
             this.pictureBoxYdre4 = new System.Windows.Forms.PictureBox();
             this.pictureBoxYdre2 = new System.Windows.Forms.PictureBox();
-            this.pictureBoxIndre2 = new System.Windows.Forms.PictureBox();
-            this.pictureBoxIndre4 = new System.Windows.Forms.PictureBox();
-            this.pictureBoxIndre3 = new System.Windows.Forms.PictureBox();
-            this.pictureBoxIndre1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBlåFirkant = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGulFirkant = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGrønFirkant = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRødFirkant = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tæller = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxXLocation = new System.Windows.Forms.TextBox();
+            this.textBoxYLocation = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.buttonBegin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBall)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYdre3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYdre1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYdre4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYdre2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIndre2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIndre4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIndre3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIndre1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlåFirkant)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGulFirkant)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGrønFirkant)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRødFirkant)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxBall
             // 
-            this.pictureBoxBall.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.pictureBoxBall.Location = new System.Drawing.Point(204, 243);
+            this.pictureBoxBall.BackColor = System.Drawing.Color.White;
+            this.pictureBoxBall.Location = new System.Drawing.Point(232, 66);
             this.pictureBoxBall.Name = "pictureBoxBall";
             this.pictureBoxBall.Size = new System.Drawing.Size(10, 10);
             this.pictureBoxBall.TabIndex = 0;
@@ -62,13 +71,12 @@ namespace Ball_freemove
             // 
             // gametimer
             // 
-            this.gametimer.Enabled = true;
             this.gametimer.Interval = 10;
             this.gametimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
             // 
             // pictureBoxYdre3
             // 
-            this.pictureBoxYdre3.BackColor = System.Drawing.Color.White;
+            this.pictureBoxYdre3.BackColor = System.Drawing.Color.Red;
             this.pictureBoxYdre3.Location = new System.Drawing.Point(50, 450);
             this.pictureBoxYdre3.Name = "pictureBoxYdre3";
             this.pictureBoxYdre3.Size = new System.Drawing.Size(400, 10);
@@ -77,7 +85,7 @@ namespace Ball_freemove
             // 
             // pictureBoxYdre1
             // 
-            this.pictureBoxYdre1.BackColor = System.Drawing.Color.White;
+            this.pictureBoxYdre1.BackColor = System.Drawing.Color.Green;
             this.pictureBoxYdre1.Location = new System.Drawing.Point(50, 50);
             this.pictureBoxYdre1.Name = "pictureBoxYdre1";
             this.pictureBoxYdre1.Size = new System.Drawing.Size(400, 10);
@@ -86,7 +94,7 @@ namespace Ball_freemove
             // 
             // pictureBoxYdre4
             // 
-            this.pictureBoxYdre4.BackColor = System.Drawing.Color.White;
+            this.pictureBoxYdre4.BackColor = System.Drawing.Color.Blue;
             this.pictureBoxYdre4.Location = new System.Drawing.Point(50, 60);
             this.pictureBoxYdre4.Name = "pictureBoxYdre4";
             this.pictureBoxYdre4.Size = new System.Drawing.Size(10, 390);
@@ -95,59 +103,145 @@ namespace Ball_freemove
             // 
             // pictureBoxYdre2
             // 
-            this.pictureBoxYdre2.BackColor = System.Drawing.Color.White;
+            this.pictureBoxYdre2.BackColor = System.Drawing.Color.Yellow;
             this.pictureBoxYdre2.Location = new System.Drawing.Point(440, 60);
             this.pictureBoxYdre2.Name = "pictureBoxYdre2";
             this.pictureBoxYdre2.Size = new System.Drawing.Size(10, 390);
             this.pictureBoxYdre2.TabIndex = 4;
             this.pictureBoxYdre2.TabStop = false;
             // 
-            // pictureBoxIndre2
+            // pictureBoxBlåFirkant
             // 
-            this.pictureBoxIndre2.BackColor = System.Drawing.Color.White;
-            this.pictureBoxIndre2.Location = new System.Drawing.Point(296, 119);
-            this.pictureBoxIndre2.Name = "pictureBoxIndre2";
-            this.pictureBoxIndre2.Size = new System.Drawing.Size(100, 50);
-            this.pictureBoxIndre2.TabIndex = 5;
-            this.pictureBoxIndre2.TabStop = false;
+            this.pictureBoxBlåFirkant.BackColor = System.Drawing.Color.Blue;
+            this.pictureBoxBlåFirkant.Location = new System.Drawing.Point(314, 119);
+            this.pictureBoxBlåFirkant.Name = "pictureBoxBlåFirkant";
+            this.pictureBoxBlåFirkant.Size = new System.Drawing.Size(82, 50);
+            this.pictureBoxBlåFirkant.TabIndex = 5;
+            this.pictureBoxBlåFirkant.TabStop = false;
             // 
-            // pictureBoxIndre4
+            // pictureBoxGulFirkant
             // 
-            this.pictureBoxIndre4.BackColor = System.Drawing.Color.White;
-            this.pictureBoxIndre4.Location = new System.Drawing.Point(130, 289);
-            this.pictureBoxIndre4.Name = "pictureBoxIndre4";
-            this.pictureBoxIndre4.Size = new System.Drawing.Size(50, 120);
-            this.pictureBoxIndre4.TabIndex = 6;
-            this.pictureBoxIndre4.TabStop = false;
+            this.pictureBoxGulFirkant.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBoxGulFirkant.Location = new System.Drawing.Point(130, 279);
+            this.pictureBoxGulFirkant.Name = "pictureBoxGulFirkant";
+            this.pictureBoxGulFirkant.Size = new System.Drawing.Size(50, 126);
+            this.pictureBoxGulFirkant.TabIndex = 6;
+            this.pictureBoxGulFirkant.TabStop = false;
             // 
-            // pictureBoxIndre3
+            // pictureBoxGrønFirkant
             // 
-            this.pictureBoxIndre3.BackColor = System.Drawing.Color.White;
-            this.pictureBoxIndre3.Location = new System.Drawing.Point(283, 255);
-            this.pictureBoxIndre3.Name = "pictureBoxIndre3";
-            this.pictureBoxIndre3.Size = new System.Drawing.Size(90, 90);
-            this.pictureBoxIndre3.TabIndex = 7;
-            this.pictureBoxIndre3.TabStop = false;
+            this.pictureBoxGrønFirkant.BackColor = System.Drawing.Color.Green;
+            this.pictureBoxGrønFirkant.Location = new System.Drawing.Point(283, 255);
+            this.pictureBoxGrønFirkant.Name = "pictureBoxGrønFirkant";
+            this.pictureBoxGrønFirkant.Size = new System.Drawing.Size(90, 90);
+            this.pictureBoxGrønFirkant.TabIndex = 7;
+            this.pictureBoxGrønFirkant.TabStop = false;
             // 
-            // pictureBoxIndre1
+            // pictureBoxRødFirkant
             // 
-            this.pictureBoxIndre1.BackColor = System.Drawing.Color.White;
-            this.pictureBoxIndre1.Location = new System.Drawing.Point(92, 161);
-            this.pictureBoxIndre1.Name = "pictureBoxIndre1";
-            this.pictureBoxIndre1.Size = new System.Drawing.Size(150, 30);
-            this.pictureBoxIndre1.TabIndex = 8;
-            this.pictureBoxIndre1.TabStop = false;
+            this.pictureBoxRødFirkant.BackColor = System.Drawing.Color.Red;
+            this.pictureBoxRødFirkant.Location = new System.Drawing.Point(114, 136);
+            this.pictureBoxRødFirkant.Name = "pictureBoxRødFirkant";
+            this.pictureBoxRødFirkant.Size = new System.Drawing.Size(128, 55);
+            this.pictureBoxRødFirkant.TabIndex = 8;
+            this.pictureBoxRødFirkant.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(410, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // Tæller
+            // 
+            this.Tæller.Interval = 1000;
+            this.Tæller.Tick += new System.EventHandler(this.Tæller_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 10;
+            // 
+            // textBoxXLocation
+            // 
+            this.textBoxXLocation.Location = new System.Drawing.Point(15, 479);
+            this.textBoxXLocation.Name = "textBoxXLocation";
+            this.textBoxXLocation.Size = new System.Drawing.Size(100, 20);
+            this.textBoxXLocation.TabIndex = 11;
+            // 
+            // textBoxYLocation
+            // 
+            this.textBoxYLocation.Location = new System.Drawing.Point(121, 479);
+            this.textBoxYLocation.Name = "textBoxYLocation";
+            this.textBoxYLocation.Size = new System.Drawing.Size(100, 20);
+            this.textBoxYLocation.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(227, 482);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Angiv startposition";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.ForeColor = System.Drawing.Color.White;
+            this.labelX.Location = new System.Drawing.Point(56, 463);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(14, 13);
+            this.labelX.TabIndex = 14;
+            this.labelX.Text = "X";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.ForeColor = System.Drawing.Color.White;
+            this.labelY.Location = new System.Drawing.Point(166, 463);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(14, 13);
+            this.labelY.TabIndex = 15;
+            this.labelY.Text = "Y";
+            // 
+            // buttonBegin
+            // 
+            this.buttonBegin.Location = new System.Drawing.Point(367, 477);
+            this.buttonBegin.Name = "buttonBegin";
+            this.buttonBegin.Size = new System.Drawing.Size(61, 22);
+            this.buttonBegin.TabIndex = 16;
+            this.buttonBegin.Text = "Begin";
+            this.buttonBegin.UseVisualStyleBackColor = true;
+            this.buttonBegin.Click += new System.EventHandler(this.buttonBegin_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(538, 511);
-            this.Controls.Add(this.pictureBoxIndre1);
-            this.Controls.Add(this.pictureBoxIndre3);
-            this.Controls.Add(this.pictureBoxIndre4);
-            this.Controls.Add(this.pictureBoxIndre2);
+            this.ClientSize = new System.Drawing.Size(502, 511);
+            this.Controls.Add(this.buttonBegin);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxYLocation);
+            this.Controls.Add(this.textBoxXLocation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxRødFirkant);
+            this.Controls.Add(this.pictureBoxGrønFirkant);
+            this.Controls.Add(this.pictureBoxGulFirkant);
+            this.Controls.Add(this.pictureBoxBlåFirkant);
             this.Controls.Add(this.pictureBoxYdre2);
             this.Controls.Add(this.pictureBoxYdre4);
             this.Controls.Add(this.pictureBoxYdre1);
@@ -160,11 +254,12 @@ namespace Ball_freemove
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYdre1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYdre4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYdre2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIndre2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIndre4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIndre3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIndre1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlåFirkant)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGulFirkant)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGrønFirkant)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRødFirkant)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -176,10 +271,19 @@ namespace Ball_freemove
         private System.Windows.Forms.PictureBox pictureBoxYdre1;
         private System.Windows.Forms.PictureBox pictureBoxYdre4;
         private System.Windows.Forms.PictureBox pictureBoxYdre2;
-        private System.Windows.Forms.PictureBox pictureBoxIndre2;
-        private System.Windows.Forms.PictureBox pictureBoxIndre4;
-        private System.Windows.Forms.PictureBox pictureBoxIndre3;
-        private System.Windows.Forms.PictureBox pictureBoxIndre1;
+        private System.Windows.Forms.PictureBox pictureBoxBlåFirkant;
+        private System.Windows.Forms.PictureBox pictureBoxGulFirkant;
+        private System.Windows.Forms.PictureBox pictureBoxGrønFirkant;
+        private System.Windows.Forms.PictureBox pictureBoxRødFirkant;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Tæller;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxXLocation;
+        private System.Windows.Forms.TextBox textBoxYLocation;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Button buttonBegin;
     }
 }
 
--- Ball freemove/Form1.cs
+++ Ball freemove/Form1.cs
@@ -7,95 +7,174 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Ball_freemove
 {
     public partial class Form1 : Form
     {
-        int ballx = 2;
-        int bally = 2;
+        int ballx = 1;
+        int bally = 1;
+        int tæller = 0;
+        int rødFirkant = 0;
+        int blåFirkant = 0;
+        int grønFirkant = 0;
+        int gulFirkant = 0;
 
-        Random rnd = new Random();
+        SoundPlayer indreMur = new SoundPlayer(@"C:\Users\zbcmber\Desktop\clank-cartoon.wav");
+        SoundPlayer ydreMur = new SoundPlayer(@"C:\Users\zbcmber\Desktop\pipe9.wav");
 
         public Form1()
         {
             InitializeComponent();
-            gametimer.Start();
         }
 
         private void mainGameTimerEvent(object sender, EventArgs e)
         {
             pictureBoxBall.Left += ballx;
-            pictureBoxBall.Top += bally; 
+            pictureBoxBall.Top += bally;
 
             if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxYdre1.Bounds))
+            {
                 bally = -bally;
+                ydreMur.Play();
+            }
 
             if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxYdre2.Bounds))
+            {
                 ballx = -ballx;
+                ydreMur.Play();
+            }
 
             if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxYdre3.Bounds))
+            {
                 bally = -bally;
+                ydreMur.Play();
+            }
 
             if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxYdre4.Bounds))
+            {
                 ballx = -ballx;
-
-
-            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxIndre1.Bounds))
-            {
-                if (pictureBoxBall.Location.X > 242) // 242
-                {
-                    ballx = -ballx;
-                }
-                else if (pictureBoxBall.Location.X < pictureBoxIndre1.Location.X)
-                {
-                    ballx = -ballx;
-                }
-                else
-                    bally = -bally;
+                ydreMur.Play();
             }
 
-            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxIndre2.Bounds))
+
+            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxRødFirkant.Bounds)) //INDRE MUR 1
             {
-                if (pictureBoxBall.Location.X > 396)// 396
+                rødFirkant++;
+                indreMur.Play();
+                if (pictureBoxBall.Location.X == 241) // 242 højre side
                 {
                     ballx = -ballx;
                 }
-                else if (pictureBoxBall.Location.X < pictureBoxIndre2.Location.X)
+                else if (pictureBoxBall.Location.X < pictureBoxRødFirkant.Location.X)
                 {
-                    ballx = -ballx;
+                    if (pictureBoxBall.Location.Y < 127 || pictureBoxBall.Location.Y == 190) // 191                    
+                        bally = -bally;
+                    else
+                        ballx = -ballx;
                 }
-                else
+                else if (pictureBoxBall.Location.Y < pictureBoxRødFirkant.Location.Y || pictureBoxBall.Location.Y == 190)// 191
+                {
                     bally = -bally;
+                }
             }
 
-            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxIndre3.Bounds))
+            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxBlåFirkant.Bounds)) //INDRE MUR 2
             {
-                if (pictureBoxBall.Location.X > 373)// 373
+                blåFirkant++;
+                indreMur.Play();
+                if (pictureBoxBall.Location.X == 395)// 396 højre side
                 {
                     ballx = -ballx;
                 }
-                else if (pictureBoxBall.Location.X < pictureBoxIndre3.Location.X)
+                else if (pictureBoxBall.Location.X < pictureBoxBlåFirkant.Location.X)
                 {
-                    ballx = -ballx;
+                    if (pictureBoxBall.Location.Y < 111 || pictureBoxBall.Location.Y == 168)// 169                    
+                        bally = -bally;
+                    else
+                        ballx = -ballx;
                 }
-                else
+                else if (pictureBoxBall.Location.Y < pictureBoxBlåFirkant.Location.Y || pictureBoxBall.Location.Y == 168)// 169
+                {
                     bally = -bally;
+                }
             }
 
-            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxIndre4.Bounds))
+            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxGrønFirkant.Bounds)) //INDRE MUR 3
             {
-                if (pictureBoxBall.Location.X > 180) // 180
+                grønFirkant++;
+                indreMur.Play();
+                if (pictureBoxBall.Location.X == 372)// 373 højre side
                 {
                     ballx = -ballx;
                 }
-                else if (pictureBoxBall.Location.X < pictureBoxIndre4.Location.X)
+                else if (pictureBoxBall.Location.X < pictureBoxGrønFirkant.Location.X)
                 {
-                    ballx = -ballx;
+                    if (pictureBoxBall.Location.Y < 248 || pictureBoxBall.Location.Y == 344)// 345            
+                        bally = -bally;
+                    else
+                        ballx = -ballx;
                 }
-                else
+                else if (pictureBoxBall.Location.Y < pictureBoxGrønFirkant.Location.Y || pictureBoxBall.Location.Y == 344)// 345             
+                {
                     bally = -bally;
+                }
             }
+
+            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxGulFirkant.Bounds)) //INDRE MUR 4
+            {
+                gulFirkant++;
+                indreMur.Play();
+                if (pictureBoxBall.Location.X == 179) // 180 højre side
+                {
+                    ballx = -ballx;
+                }
+                else if (pictureBoxBall.Location.X < pictureBoxGulFirkant.Location.X)
+                {
+                    if (pictureBoxBall.Location.Y < 272 || pictureBoxBall.Location.Y == 404)// 405
+                        bally = -bally;
+                    else
+                        ballx = -ballx;
+                }
+                else if (pictureBoxBall.Location.Y < pictureBoxGulFirkant.Location.Y || pictureBoxBall.Location.Y == 404) // 405                
+                {
+                    bally = -bally;
+                }
+            }
+            label2.Text = "Rød firkant: " + rødFirkant.ToString() + ".        " + "Blå firkant: " + blåFirkant.ToString() + ".\n" + "Gul firkant: " + gulFirkant.ToString() + ".         " + "Grøn firkant: " + grønFirkant.ToString() + ". ";
+        }
+
+        private void Tæller_Tick(object sender, EventArgs e)
+        {
+            tæller++;
+            label1.Text = "Sekunder: " + TimeSpan.FromSeconds(tæller).ToString("ss");
+        }
+
+        private void buttonBegin_Click(object sender, EventArgs e)
+        {
+            int xInput = Convert.ToInt32(textBoxXLocation.Text);
+            int yInput = Convert.ToInt32(textBoxYLocation.Text);
+            pictureBoxBall.Top = yInput;
+            pictureBoxBall.Left = xInput;
+            if (pictureBoxBall.Bounds.IntersectsWith(pictureBoxBlåFirkant.Bounds) || pictureBoxBall.Bounds.IntersectsWith(pictureBoxRødFirkant.Bounds) || pictureBoxBall.Bounds.IntersectsWith(pictureBoxGrønFirkant.Bounds) || pictureBoxBall.Bounds.IntersectsWith(pictureBoxGulFirkant.Bounds))
+            {
+                gametimer.Stop();
+                Tæller.Stop();
+                MessageBox.Show("You can't hit a colored box.. Try again");
+            }
+            else
+            {
+                gametimer.Start();
+                Tæller.Start();
+            }
+            tæller = 0;
+            int rødFirkant = 0;
+            int blåFirkant = 0;
+            int grønFirkant = 0;
+            int gulFirkant = 0;
+            label1.Text = "Sekunder: " + TimeSpan.FromSeconds(tæller).ToString("ss");
+            label2.Text = "Rød firkant: " + rødFirkant.ToString() + ".        " + "Blå firkant: " + blåFirkant.ToString() + ".\n" + "Gul firkant: " + gulFirkant.ToString() + ".         " + "Grøn firkant: " + grønFirkant.ToString() + ". ";
         }
     }
 }
